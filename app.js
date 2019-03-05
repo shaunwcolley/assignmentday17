fc7493225c88ff0b0b3565dfa405bbe3f8f56c06
Shaun W Colley <swc7838@gmail.com>
2019-03-05 15:41:15 -0600

    some formatting, still needs more, also app works properly

--- app.js
+++ app.js
@@ -7,37 +7,34 @@ btnAddTask.addEventListener('click', function () {
 
   let taskName = taskNameTextBox.value
   let taskListItem = document.createElement("li")
-  taskListItem.append(taskName)
   let checkBox = document.createElement("input")
   checkBox.type = "checkbox"
-  pendingTasksList.append(checkBox)
-  pendingTasksList.append(taskListItem)
   let deleteButton = document.createElement("button")
-  pendingTasksList.append(deleteButton)
-  deleteButton.innerHTML = "Remove"
+  if(taskName == "" || taskName == " " || taskName == "  ") {
+    alert("Please enter text when making a task.")
+  } else {
+    pendingTasksList.append(taskListItem)
+    taskListItem.append(checkBox)
+    taskListItem.append(taskName)
+    taskListItem.append(deleteButton)
+    deleteButton.innerHTML = "Remove"
+  }
+
   checkBox.addEventListener('change', function() {
     if(this.checked) {
-      completedTasksList.append(checkBox)
       completedTasksList.append(taskListItem)
-      completedTasksList.append(deleteButton)
     }
     else {
-      pendingTasksList.append(checkBox)
       pendingTasksList.append(taskListItem)
-      pendingTasksList.append(deleteButton)
     }
 
   })
   deleteButton.addEventListener('click', function() {
     if(pendingTasksList.contains(taskListItem)) {
       pendingTasksList.removeChild(taskListItem)
-      pendingTasksList.removeChild(deleteButton)
-      pendingTasksList.removeChild(checkBox)
     }
     else if (completedTasksList.contains(taskListItem)){
     completedTasksList.removeChild(taskListItem)
-    completedTasksList.removeChild(deleteButton)
-    completedTasksList.removeChild(checkBox)
     }
   })
 })
